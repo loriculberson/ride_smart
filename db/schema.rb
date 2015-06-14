bb671e5b1604094b5e25f09dd36237bb54c0faa9
Lori Culberson <bigskybandit@sbcglobal.net>
2015-06-13 19:06:05 -0600

    Add twitter nickname and provider to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613235007) do
+ActiveRecord::Schema.define(version: 20150614005748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "twitter_username"
+    t.string   "nickname"
     t.string   "email"
     t.string   "password_digest"
     t.string   "token"
-    t.string   "image"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.string   "image_url"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "provider"
   end
 
 end
