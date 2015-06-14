6924373c4acacadf3356c20dec9e04cb5d177040
Lori Culberson <bigskybandit@sbcglobal.net>
2015-06-14 10:15:27 -0600

    Add create to Users controller, modified new user form

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -9,5 +9,18 @@ RSpec.describe type: :feature do
     expect(current_path).to eq(new_user_path)
   end
 
+  it "sees successful flash message if user is created" do
+    visit new_user_path
+    fill_in("user[username]", with: "buttercup" )
+    fill_in("user[password]", with: "password" )
+    fill_in("user[password_confirmation]", with: "password" )
+    fill_in("user[email]", with: "happy@example.com" )
+    fill_in("user[display_name]", with: "rockstar" )
+    click_link_or_button "Create Account"
+
+    expect(page).to have_content("Account created!")
+
+  end
+
   
 end
