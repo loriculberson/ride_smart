71bad70469a9a23b948e1cda986640d93b59f343
Lori Culberson <bigskybandit@sbcglobal.net>
2015-06-13 18:01:36 -0600

    Add home controller

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,6 +1,11 @@
+require 'rails_helper'
+
 RSpec.describe type: :feature do
 
   it "can create and account" do
-    visit
+    visit '/'
+    click 'Sign Up'
+
+    expect(current_path).to eq(new_user_path)
   end
 end
