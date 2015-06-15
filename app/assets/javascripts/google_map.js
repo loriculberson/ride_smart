ca783ff852357baa0a6b6be93b145730523d85ad
Lori Culberson <bigskybandit@sbcglobal.net>
2015-06-15 13:44:43 -0600

    Redesigned navbar for homepage

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -34,26 +34,26 @@ $(document).ready(function(){
     types: ['establishment']
   };
 
-  var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
-  autocomplete.bindTo('bounds',map);
+  // var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
+  // autocomplete.bindTo('bounds',map);
 
-  google.maps.event.addListener(autocomplete, 'place_changed', function(){
-    infoWindow.close();
-    var place = autocomplete.getPlace();
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
-    marker.setPosition(place.geometry.location);
-    infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
-    infoWindow.open(map, marker);
-    google.maps.event.addListener(marker, 'click', function(e){
+  // google.maps.event.addListener(autocomplete, 'place_changed', function(){
+  //   infoWindow.close();
+  //   var place = autocomplete.getPlace();
+  //   if (place.geometry.viewport) {
+  //     map.fitBounds(place.geometry.viewport);
+  //   } else {
+  //     map.setCenter(place.geometry.location);
+  //     map.setZoom(17);
+  //   }
+  //   marker.setPosition(place.geometry.location);
+  //   infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+  //   infoWindow.open(map, marker);
+  //   google.maps.event.addListener(marker, 'click', function(e){
 
-      infoWindow.open(map, marker);
+  //     infoWindow.open(map, marker);
 
-    });
+  //   });
 
 
 
--- app/controllers/bike_events_controller.rb
+++ app/controllers/bike_events_controller.rb
@@ -1,4 +1,7 @@
 class BikeEventsController < ApplicationController
 
+  def index
+  end
+
 
 end
