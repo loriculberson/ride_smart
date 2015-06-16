0b7bf5daa623d881a64202f667dafe17e94fdc1a
Lori Culberson <bigskybandit@sbcglobal.net>
2015-06-16 09:31:12 -0600

    Add uid to user model and WIP add omniauth google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #if the user logs in with Google
+    user = User.find_or_create_from_auth(auth)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in!"
+      redirect_to root_path
+    else
+      flash[:danger] = "Invalid login. Please try again."
+      redirect_to :back
+    end
+
+    #if the user logs in with email
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
@@ -20,5 +32,11 @@ class SessionsController < ApplicationController
     session.clear
     flash[:danger] = "Successfully logged out!"
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
   
   has_secure_password
+
+  def self.find_or_create_from_auth(auth)
+    User.find_or_create_by(provider: auth.provider, uid: auth.uid).tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.name
+      user.email = auth.info.email
+      user.save!
+    end
+  end
 end
