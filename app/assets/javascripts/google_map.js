0e365f0b41e9dbad44dddace0de3dddc51437901
Lori Culberson <bigskybandit@sbcglobal.net>
2015-06-17 13:05:39 -0600

    WIP map basics

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,62 +1,110 @@
 $(document).ready(function(){
 
-//setting up the map, the map options and the starting position
+// setting up the map, the map options and the starting position
   var mapCanvas = document.getElementById('map-canvas');
   
   var mapOptions = {
     center: new google.maps.LatLng(39.7392, -104.9903),
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
+  };
   
   var map = new google.maps.Map(mapCanvas, mapOptions);
 
-//marker on the map -- position and setting the marker
-  var markerOptions = {
-    position: new google.maps.LatLng(39.750444, -104.999489)
+//setting the map boundaries
+  var defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(39.7392, -104.9903),
+    new google.maps.LatLng(39.7392, -104.9903)
+  );
+
+//html input element for autocomplete search box
+  var input = document.getElementById('autocomplete');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // autocomplete box and content
+  var acOptions = {
+    bounds:defaultBounds  
   };
+// Create the autocomplete object
+  var autocomplete = new google.maps.places.Autocomplete(input, acOptions);
+  autocomplete.bindTo('bounds', map);
 
-  var marker = new google.maps.Marker(markerOptions);
+  var infoWindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map
 
-  marker.setMap(map)
+  });
 
-//pop up window
-  var infoWindowOptions = {
-    content: 'Blake Street Vault -- Drink here!'
-  };
+  google.maps.event.addListener(autocomplete, 'place_changed',function(){
+    infoWindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert( "Sorry, there are no places that meet your search");
+      return;
+    }
 
-  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-  google.maps.event.addListener(marker, 'click', function(e){
-    infoWindow.open(map, marker);
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+    marker.setIcon(({
+      
+    }))
+
   })
 
-//autocomplete box and content
-  var acOptions = {
-    types: ['establishment']
-  };
-
-  // var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
-  // autocomplete.bindTo('bounds',map);
-
-  // google.maps.event.addListener(autocomplete, 'place_changed', function(){
-  //   infoWindow.close();
-  //   var place = autocomplete.getPlace();
-  //   if (place.geometry.viewport) {
-  //     map.fitBounds(place.geometry.viewport);
-  //   } else {
-  //     map.setCenter(place.geometry.location);
-  //     map.setZoom(17);
-  //   }
-  //   marker.setPosition(place.geometry.location);
-  //   infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
-  //   infoWindow.open(map, marker);
-  //   google.maps.event.addListener(marker, 'click', function(e){
-
-  //     infoWindow.open(map, marker);
-
-  //   });
+  // });
+  });
 
 
 
 
-});
+
+// //marker on the map -- position and setting the marker
+//   var markerOptions = {
+//     position: new google.maps.LatLng(39.750444, -104.999489)
+//   };
+
+//   var marker = new google.maps.Marker(markerOptions);
+
+//   marker.setMap(map)
+
+// 
+
+
+
+
+
+
+//pop up window
+//   var infoWindowOptions = {
+//     content: 'Blake Street Vault -- Drink here!'
+//   };
+
+//   var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+  
+//   google.maps.event.addListener(marker, 'click', function(e){
+//     infoWindow.open(map, marker);
+//   })
+
+//   google.maps.event.addListener(autocomplete, 'place_changed', function(){
+//     infoWindow.close();
+//     var place = autocomplete.getPlace();
+//     if (place.geometry.viewport) {
+//       map.fitBounds(place.geometry.viewport);
+//     } else {
+//       map.setCenter(place.geometry.location);
+//       map.setZoom(17);
+//     }
+    
+//     marker.setPosition(place.geometry.location);
+//     infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+//     infoWindow.open(map, marker);
+//     google.maps.event.addListener(marker, 'click', function(e){
+
+//       infoWindow.open(map, marker);
+
+//     });
