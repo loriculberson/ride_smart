f986f28b3fbab5df7c5655f3ac1c79655031f821
Lori Culberson <bigskybandit@sbcglobal.net>
2015-06-25 11:04:08 -0600

    Logout tests

--- spec/features/user_login_logout_spec.rb
+++ spec/features/user_login_logout_spec.rb
@@ -9,19 +9,22 @@ RSpec.describe "authorized user can login", type: :feature do
     click_on 'with Email'
   end
 
-  it "can login" do
-    login
-    
+  def fill_in_login
     fill_in("session[email]", with: "bob@example.com" )
     fill_in("session[password]", with: "password" )
     click_button 'Login'
+  end
+
+  it "can login" do
+    login
+    fill_in_login
 
     expect(page).to have_content("Successfully logged in!")
     expect(page).to have_content("Hello #{@user.username}!")
     expect(current_path).to eql(bike_events_path)
   end
 
-  xit "can not login without a valid password" do
+  it "can not login without a valid password" do
     login
 
     fill_in("session[email]", with: "bob@example.com" )
@@ -31,4 +34,13 @@ RSpec.describe "authorized user can login", type: :feature do
     expect(page).to have_content("Invalid login. Please try again.")
     expect(current_path).to eql(login_path)
   end  
+
+  it "can logout" do 
+    login
+    fill_in_login
+    click_on "Logout"
+
+    expect(page).to have_content("Successfully logged out!")
+  end
+
 end
