3626d807e807179cd8958216d06052d507b289e3
Lori Culberson <bigskybandit@sbcglobal.net>
2016-06-24 21:49:20 -0500

    Uniform image size in infowindow Add default image to infowindow sans image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,13 +1,10 @@
 class AvatarUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
-  process resize_to_fit: [800, 800] 
- 
+  process resize_to_fit: [800, 800]
+
   storage :aws
-  # storage :file
-  # permissions 0777
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
@@ -16,16 +13,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("bike_marker.png")
+  end
+
   version :thumb do
     process :resize_to_fit => [150, 150]
   end
-
-  version :preview do
-    process :resize_to_fit => [256, 256]
-  end
-
-  version :full do
-    process :resize_to_fit => [2048, 2048]
-  end
-
 end
